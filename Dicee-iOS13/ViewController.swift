e5d07c0cfe56867cfa9db2d09d9da64601d98807
Brenna Pavlinchak <raven.mobile.design@gmail.com>
2022-01-02 18:37:24 -0500

    Udemy Swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var diceImageOne: UIImageView!
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    var leftDice = 1
+    var rightDice = 1
 
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any)
+    {
+        diceImageOne.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][leftDice]
+        diceImageTwo.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix") ][rightDice]
+        
+        leftDice = leftDice + 1
+        rightDice = rightDice + 1
+    }
+    
 }
 
